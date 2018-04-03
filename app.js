d690541411b2f370a1998bd99ddd91684ea4cbf4
Juho Vainio <Juho.Vainio@student.laurea.fi>
2018-04-03 12:28:26 +0300

    fixed typo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 //require('./app_server/models/db');
-require('.app_api/models/db');
+require('./app_api/models/db');
 
 const index = require('./app_server/routes/index');
 const users = require('./app_server/routes/users');
